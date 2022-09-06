86f2bfec236c8e4818ea514c171b1cbe96f8eaae
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-09-06 11:45:47 +0545

    feat: app created with db connection

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const { Sequelize, QueryTypes } = require('sequelize')
+const app = express()
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
+
+app.get('/api/notes', async (req, res) => {
+  const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
+  res.json(notes)
+})
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
