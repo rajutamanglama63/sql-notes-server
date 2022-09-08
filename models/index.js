60ed6e22ecb1189be82c37d17f2f5a09f88e6774
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-09-08 14:54:38 +0545

    feat: bug fixed

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 const Note = require("./Note");
 const User = require("./User");
 
-Note.sync();
-User.sync();
+User.hasMany(Note)
+Note.belongsTo(User)
+Note.sync({alter: true});
+User.sync({alter: true});
 
 module.exports = {
   Note,
