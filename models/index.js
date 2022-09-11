58909de8639cfa128c719f350a0445d309545b98
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-09-11 23:23:35 +0545

    feat: migrations file

--- models/index.js
+++ models/index.js
@@ -1,12 +1,21 @@
 const Note = require("./Note");
 const User = require("./User");
+const Team = require("./team")
+const Membership = require("./membership")
 
 User.hasMany(Note);
 Note.belongsTo(User);
+
+User.belongsToMany(Team, { through: Membership })
+Team.belongsToMany(User, { through: Membership })
+
+
 // Note.sync({alter: true});
 // User.sync({alter: true});
 
 module.exports = {
   Note,
   User,
+  Team,
+  Membership
 };
